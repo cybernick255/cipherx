61c82082cd0c0eeb77b3e52f0913c4e2855da41e
Nicolas Deleasa <nicktakesmail@gmail.com>
2025-08-04 15:38:53 -0400

    Initial Commit

---
+++ CipherX/CipherXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CipherXApp.swift
+//  CipherX
+//
+//  Created by Nicolas Deleasa on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CipherXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
