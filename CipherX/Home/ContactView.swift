72645c8fbcccd2bd8ce2d77c0442cb6bd91b14f3
Nicolas Deleasa <nicktakesmail@gmail.com>
2025-08-10 16:26:16 -0400

    Version 0.1.8 - Implemented decryption functionality.

--- CipherX/Home/ContactView.swift
+++ CipherX/Home/ContactView.swift
@@ -33,7 +33,7 @@ struct ContactView: View
                         .foregroundStyle(Constants().secondaryColor)
                         .onChange(of: encryptedMessage)
                         {
-                            //self.decryptMessage()
+                            self.decryptMessage()
                         }
                     TextField("", text: $encryptedMessage)
                         .background(Color.black)
@@ -56,63 +56,45 @@ struct ContactView: View
         .navigationTitle(contact.name)
     }
     
-    // Not working
-//    func decryptMessage()
-//    {
-//        let privateKey = generatePrivateKeyFrom(string: keyPairs[0].privateKey)
-//        print(keyPairs[0].privateKey)
-//        let publicKey = generatePublicKeyFrom(string: contact.publicKey)
-//        let ciphersuite = HPKE.Ciphersuite.P384_SHA384_AES_GCM_256
-//        let protocolInfo = "Contact Message".data(using: .utf8)!
-//        let encapsulatedKey = generateEncapsulatedKey()
-//        let ciphertext = generateCiphertext()
-//        print("Encapsulated Key: \(encapsulatedKey.base64EncodedString())")
-//        print(ciphertext)
-//        let data = Data(base64Encoded: encryptedMessage)!
-//        print("Combined raw byte count: \(data.count)")
-//        print("Encapsulated key: \(data.prefix(96).base64EncodedString())")
-//        var hpkeRecipient = try! HPKE.Recipient(privateKey: privateKey, ciphersuite: ciphersuite, info: protocolInfo, encapsulatedKey: encapsulatedKey)
-//        
-//        try! self.decryptedMessage = hpkeRecipient.open(ciphertext).base64EncodedString()
-//    }
-//    
-//    func generatePrivateKeyFrom(string: String) -> P384.KeyAgreement.PrivateKey
-//    {
-//        let rawPrivateKeyData = Data(base64Encoded: string)!
-//        
-//        return try! P384.KeyAgreement.PrivateKey(rawRepresentation: rawPrivateKeyData)
-//    }
-//    
-//    func generatePublicKeyFrom(string: String) -> P384.KeyAgreement.PublicKey
-//    {
-//        let rawPublicKeyData = Data(base64Encoded: string)!
-//        
-//        return try! P384.KeyAgreement.PublicKey(rawRepresentation: rawPublicKeyData)
-//    }
-//    
-//    func generateEncryptedMessageDataFrom(string: String) -> Data
-//    {
-//        print(Data(base64Encoded: string)!)
-//        return Data(base64Encoded: string)!
-//    }
-//    
-//    func generateEncapsulatedKey() -> Data
-//    {
-//        let data = generateEncryptedMessageDataFrom(string: encryptedMessage)
-//            let encapsulatedKey = data.prefix(96)
-//
-//            print("EncapsulatedKey byte count: \(encapsulatedKey.count)")  // Should be 96
-//            print("EncapsulatedKey base64 length: \(encapsulatedKey.base64EncodedString().count)")  // Should be 128
-//            print("EncapsulatedKey base64: \(encapsulatedKey.base64EncodedString())")
-//
-//            return encapsulatedKey
-//        //return generateEncryptedMessageDataFrom(string: encryptedMessage).prefix(96)
-//    }
-//    
-//    func generateCiphertext() -> Data
-//    {
-//        return generateEncryptedMessageDataFrom(string: encryptedMessage).suffix(from: 96)
-//    }
+    func decryptMessage()
+    {
+        let encryptedMessage = importEncryptedMessage(encryptedMessage)
+        
+        let privateKey = generatePrivateKeyFrom(string: keyPairs[0].privateKey)
+        let ciphersuite = HPKE.Ciphersuite.P384_SHA384_AES_GCM_256
+        let protocolInfo = "Messages".data(using: .utf8)!
+        let encapsulatedKey = encryptedMessage.encapsulatedKey
+        
+        var hpkeRecipient =
+        try! HPKE.Recipient(
+            privateKey: privateKey,
+            ciphersuite: ciphersuite,
+            info: protocolInfo,
+            encapsulatedKey: encapsulatedKey
+        )
+        
+        let decryptedCiphertextData = try! hpkeRecipient.open(encryptedMessage.ciphertext)
+        let decryptedCiphertextString = String(data: decryptedCiphertextData, encoding: .utf8)!
+        
+        decryptedMessage = decryptedCiphertextString
+    }
+    
+    func generatePrivateKeyFrom(string: String) -> P384.KeyAgreement.PrivateKey
+    {
+        let rawPrivateKeyData = Data(base64Encoded: string)!
+        
+        return try! P384.KeyAgreement.PrivateKey(rawRepresentation: rawPrivateKeyData)
+    }
+    
+    func importEncryptedMessage(_ encryptedMessageString: String) -> EncryptedMessage
+    {
+        let parts = encryptedMessageString.split(separator: ",")
+        
+        let ciphertextData = Data(base64Encoded: String(parts[0]))!
+        let encapsulatedKeyData = Data(base64Encoded: String(parts[1]))!
+        
+        return EncryptedMessage(ciphertext: ciphertextData, encapsulatedKey: encapsulatedKeyData)
+    }
 }
 
 #Preview
---
+++ CipherX/Models/EncryptedMessage.swift
@@ -0,0 +1,14 @@
+//
+//  EncryptedMessage.swift
+//  CipherX
+//
+//  Created by Nicolas Deleasa on 8/10/25.
+//
+
+import Foundation
+
+struct EncryptedMessage
+{
+    let ciphertext: Data
+    let encapsulatedKey: Data
+}
