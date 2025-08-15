0bc0adc97c3345d2281245d61cad4566fe508787
Nicolas Deleasa <nicktakesmail@gmail.com>
2025-08-15 17:03:32 -0400

    Version 0.2 - Implemented import KeyPair functionality. Implemented "App Uses Non-Exempt Encryption" key in "Info.plist" with the value "NO".

--- CipherX/Welcome/ImportKeys/ImportKeysView.swift
+++ CipherX/Welcome/ImportKeys/ImportKeysView.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import CryptoKit
 
 struct ImportKeysView: View
 {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var privateKeyString: String = ""
+    @State private var errorMessage: String?
+    @State private var alertPresented: Bool = false
+    
     var body: some View
     {
         ZStack
@@ -17,8 +24,96 @@ struct ImportKeysView: View
                 .ignoresSafeArea()
                 .foregroundStyle(Constants().primaryColor)
             
-            Text("Import Keys")
-                .foregroundStyle(Constants().secondaryColor)
+            VStack
+            {
+                HStack
+                {
+                    Text("Private Key")
+                        .foregroundStyle(Constants().secondaryColor)
+                    Spacer()
+                }
+                TextField("Private Key", text: $privateKeyString)
+                    .background(Color.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                Button(action: submitKey)
+                {
+                    Text("Submit")
+                        .padding()
+                        .background(Capsule().foregroundStyle(Constants().secondaryColor))
+                        .foregroundStyle(.white)
+                }
+                .padding(.top)
+            }
+            .padding()
+        }
+        .alert("Error", isPresented: $alertPresented)
+        {
+            Button("Okay", role: .cancel)
+            {
+                self.errorMessage = nil
+            }
+        }
+    message:
+        {
+            Text(errorMessage ?? "Unknown error.")
+        }
+    }
+    
+    func submitKey()
+    {
+        do
+        {
+            let privateKey = try importPrivateKey(privateKeyString)
+            let publicKey = privateKey.publicKey
+            let keyPair = KeyPair(privateKey: privateKey.rawRepresentation.base64EncodedString(), publicKey: publicKey.rawRepresentation.base64EncodedString())
+            
+            self.saveKeyPair(keyPair)
+        }
+        catch
+        {
+            self.errorMessage = error.localizedDescription
+            self.alertPresented = true
+        }
+    }
+    
+    func importPrivateKey(_ base64EncodedString: String) throws -> P384.KeyAgreement.PrivateKey
+    {
+        guard let data = Data(base64Encoded: base64EncodedString)
+        else
+        {
+            throw ImportPrivateKeyError.nilKeyData
+        }
+        
+        do
+        {
+            let key = try P384.KeyAgreement.PrivateKey(rawRepresentation: data)
+            return key
+        }
+        catch
+        {
+            throw ImportPrivateKeyError.invalidKeyData
+        }
+    }
+    
+    func saveKeyPair(_ keyPair: KeyPair)
+    {
+        modelContext.insert(keyPair)
+    }
+    
+    enum ImportPrivateKeyError: LocalizedError
+    {
+        case invalidKeyData
+        case nilKeyData
+        
+        var errorDescription: String?
+        {
+            switch self
+            {
+            case .invalidKeyData:
+                return "Invalid key data. (0)"
+            case .nilKeyData:
+                return "Invalid key data. (1)"
+            }
         }
     }
 }
